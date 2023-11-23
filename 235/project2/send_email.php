915ea0f94b67d1f276c73463f5c5eaac1dadd51e
Jason Chen <c04jason@gmail.com>
2023-11-23 02:12:12 -0500

    started on docs

---
+++ 235/project2/send_email.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST['subject'])) {
+    $to = 'jc5892@rit.edu';
+    $subject = $_POST['subject'];
+    $message = 'Name: ' . $_POST['firstname'] . ' ' . $_POST['lastname'] . "\r\n\r\n";
+    $message .= 'Subject: ' . $subject;
+
+    mail($to, $subject, $message);
+}
+?>
