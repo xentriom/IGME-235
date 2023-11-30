c9acecc63df4d63e7f19ad6aaff0da50795cdf70
Jason Chen <c04jason@gmail.com>
2023-11-29 20:22:32 -0500

    background music

--- 235/project3/scripts/mainScene.js
+++ 235/project3/scripts/mainScene.js
@@ -2,62 +2,86 @@ const app = new PIXI.Application({ width: window.innerWidth, height: window.inne
 document.body.appendChild(app.view);
 
 const mainScene = new PIXI.Container();
-
 app.stage.addChild(mainScene);
 
-const title = new PIXI.Text('Fruit Ninja: REmastered', { fontSize: 36, fill: 'white' });
+playBackgroundMusic();
+
+const title = new PIXI.Text('Fruit Ninja: REmastered', { fontSize: 70, fill: 'white' });
 title.x = app.screen.width / 2 - title.width / 2;
-title.y = 100;
+title.y = 200;
 mainScene.addChild(title);
 
-const startButton = createButton('Start', `./media/icons/Play.png`, 300, 200, () => {
+const startButton = createButton('Start', `./media/icons/Play.png`, 400, () => {
     console.log('Start button clicked');
 });
 mainScene.addChild(startButton);
 
-const instructionButton = createButton('Instruction', `./media/icons/Instruction.png`, 300, 300, () => {
+const instructionButton = createButton('Instruction', `./media/icons/Instruction.png`, 500, () => {
     console.log('Instruction button clicked');
 });
 mainScene.addChild(instructionButton);
 
-const shopButton = createButton('Shop', `./media/icons/Shop.png`,300, 400, () => {
+const shopButton = createButton('Shop', `./media/icons/Shop.png`, 600, () => {
     console.log('Shop button clicked');
 });
 mainScene.addChild(shopButton);
 
-// Function to create a button with text and icon
-function createButton(text, iconPath, x, y, onClick) {
+const settingsButton = createButton('Settings', `./media/icons/Gear.png`, 700, () => {
+    console.log('Settings button clicked');
+    app.stage.removeChild(mainScene);
+});
+mainScene.addChild(settingsButton);
+
+function createButton(text, iconPath, y, onClick) {
     const button = new PIXI.Container();
 
-    // Create the button background
     const buttonBg = new PIXI.Sprite(PIXI.Texture.WHITE);
-    buttonBg.width = 200;
-    buttonBg.height = 50;
-    buttonBg.tint = 0x555555; // Button color
+    buttonBg.width = 300;
+    buttonBg.height = 75;
+    buttonBg.tint = 0x555555;
     button.addChild(buttonBg);
 
-    // Create the button text
-    const buttonText = new PIXI.Text(text, { fontSize: 24, fill: 'white' });
+    const buttonText = new PIXI.Text(text, { fontSize: 30, fill: 'white' });
     buttonText.x = buttonBg.width / 2 - buttonText.width / 2;
     buttonText.y = buttonBg.height / 2 - buttonText.height / 2;
     button.addChild(buttonText);
 
-    // Create the button icon
     const icon = new PIXI.Sprite.from(iconPath);
-    icon.width = 30; // Adjust the size of the icon as needed
-    icon.height = 30;
-    icon.x = 10; // Adjust the icon position as needed
+    icon.width = 40;
+    icon.height = 40;
+    icon.x = 10;
     icon.y = buttonBg.height / 2 - icon.height / 2;
     button.addChild(icon);
 
-    // Set the button's position
-    button.x = x;
+    button.x = (app.screen.width - buttonBg.width) / 2;
     button.y = y;
 
-    // Add interactive behavior
     button.interactive = true;
     button.buttonMode = true;
     button.on('pointerdown', onClick);
 
     return button;
+}
+
+async function playBackgroundMusic() {
+    try {
+        const backgroundMusic = new Howl({
+            src: ['./media/audio/main_bgm.mp3'],
+            loop: true,
+            volume: 0.5,
+        });
+        await backgroundMusic.play();
+    } catch (error) {
+        console.error('Error playing background music:', error.message);
+    }
+}
+
+function loadScene(sceneName) {
+    const script = document.createElement('script');
+    script.src = `${sceneName}Scene.js`;
+    script.onload = () => {
+        const sceneClassName = `${sceneName.charAt(0).toUpperCase()}${sceneName.slice(1)}Scene`;
+        const sceneInstance = new window[sceneClassName](app.stage);
+    };
+    document.head.appendChild(script);
 }
