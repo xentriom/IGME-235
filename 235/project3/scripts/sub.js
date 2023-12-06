6903679867ca8b9a0f96fd45fda34f8e633fba43
Jason Chen <c04jason@gmail.com>
2023-12-06 17:28:01 -0500

    life mode + preload progress

--- 235/project3/scripts/sub.js
+++ 235/project3/scripts/sub.js
@@ -1,19 +1,13 @@
 const lives = 3;
-let mode = "";
 let score = 0;
 let trailSize = 2;
 
-class MainMenu extends Phaser.Scene {
-    constructor() { super({ key: 'MainMenu' }); }
+class LoadingScene extends Phaser.Scene {
+    constructor() { super({ key: 'LoadingScene' }); }
 
     preload() {
-        // const graphics = ['EndlessImage, TimedImage, LifeImage'];
         const veggieNames = ['BellPepper', 'Broccoli', 'Carrot', 'Cauliflower', 'Corn', 'Eggplant', 'GreenCabbage', 'Mushroom', 'Potato', 'Pumpkin', 'Radish', 'Squash', 'Tomato'];
-        const iconNames = ['Home', 'Shop', 'Instruction', 'Play', 'Gear', 'PlayPause'];
-
-        // graphics.forEach((graphic) => {
-        //     this.load.image(graphic, `./media/graphics/${graphic}.png`);
-        // });
+        const iconNames = ['Home', 'Shop', 'Instruction', 'Play', 'Gear', 'PlayPause', 'Heart', 'BrokenHeart'];
 
         veggieNames.forEach((veggieName) => {
             this.load.image(veggieName, `./media/veggies/${veggieName}.png`);
@@ -22,7 +16,74 @@ class MainMenu extends Phaser.Scene {
         iconNames.forEach((iconName) => {
             this.load.image(iconName, `./media/icons/${iconName}.png`);
         });
+
+        const progressBar = this.add.graphics();
+        const progressBox = this.add.graphics();
+        const width = this.cameras.main.width;
+        const height = this.cameras.main.height;
+        const loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+
+        const percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 50,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+
+        const assetText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 100,
+            text: '',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        assetText.setOrigin(0.5, 0.5);
+
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(width / 4, height / 2 - 30, width / 2, 50);
+
+        this.load.on('progress', function (value) {
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(width / 4 + 10, height / 2 - 20, (width / 2 - 20) * value, 30);
+            percentText.setText(parseInt(value * 100) + '%');
+        });
+
+        this.load.on('fileprogress', function (file) {
+            assetText.setText('Loading asset: ' + file.key);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            assetText.destroy();
+        });
     }
+
+    create() {
+        this.scene.start('MainMenu');
+    }
+}
+
+class MainMenu extends Phaser.Scene {
+    constructor() { super({ key: 'MainMenu' }); }
 
     create() {
         this.add.text(this.sys.game.config.width / 2, 200, 'Veggie Slicer', {
@@ -206,7 +267,6 @@ class PracticeGame extends Phaser.Scene {
 
     create() {
         console.log('PracticeGame: creating');
-        mode = "practice";
 
         this.createHomeButton();
         this.scoreField = this.createScoreField();
@@ -366,11 +426,11 @@ class TimedGame extends Phaser.Scene {
         this.trailPoints = [];
         this.currentTime = 60000;
         this.timerEvent;
+        this.score = 0;
     }
 
     create() {
         console.log('TimedGame: creating');
-        mode = "timed";
 
         this.createHomeButton();
         this.scoreField = this.createScoreField();
@@ -462,7 +522,7 @@ class TimedGame extends Phaser.Scene {
         this.timerContainer.getAt(1).setText(`${formattedTime}`);
 
         if (this.currentTime <= 0) {
-            this.scene.start('GameOver');
+            this.scene.start('GameOver', { mode: 'Timed', score: score });
             this.timerEvent.destroy();
         }
     }
@@ -563,28 +623,210 @@ class TimedGame extends Phaser.Scene {
 }
 
 class LifeGame extends Phaser.Scene {
-    constructor() { super({ key: 'LifeGame' }); }
+    constructor() {
+        super({ key: 'LifeGame' });
+        this.veggieGroup;
+        this.trailGraphics;
+        this.trailPoints = [];
+        this.lives = 3;
+        this.score = 0;
+    }
 
     create() {
         console.log('LifeGame: creating');
-        mode = "life";
+
+        this.createHomeButton();
+        this.scoreField = this.createScoreField();
+
+        this.veggieGroup = this.physics.add.group();
+
+        this.spawnVeggie();
+        this.time.addEvent({
+            delay: 500,
+            callback: this.spawnVeggie,
+            callbackScope: this,
+            loop: true,
+        });
+
+        this.trailGraphics = this.add.graphics();
+        this.input.on('pointermove', this.handleMouseMove, this);
+
+        this.hearts = [];
+        for (let i = 0; i < this.lives; i++) {
+            const heart = this.add.image(20 + i * 40, 20, 'Heart').setOrigin(0, 0).setScale(2);
+            this.hearts.push(heart);
+        }
+    }
+
+    createHomeButton() {
+        const homeButton = this.add.container(20, 20);
+        const iconKey = 'Home';
+
+        const buttonBackground = this.add.rectangle(10, 10, 50, 50, 0xffffff)
+            .setOrigin(0)
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => {
+                this.scene.start('MainMenu');
+            });
+
+        const homeIcon = this.add.image(35, 35, iconKey)
+            .setOrigin(0.5)
+            .setScale(2);
+
+        homeButton.add([buttonBackground, homeIcon]);
+
+        return homeButton;
+    }
+
+    createScoreField() {
+        score = 0;
+        const scoreField = this.add.container(this.sys.game.config.width - 20, 20);
+
+        const fieldBackground = this.add.graphics()
+            .fillStyle(0xffffff)
+            .fillRoundedRect(-220, 0, 200, 50, 10);
+
+        const scoreText = this.add.text(-50, 25, `Score: ${score}`, {
+            fontSize: '24px',
+            fill: '#000',
+        }).setOrigin(1, 0.5);
+
+        scoreField.add([fieldBackground, scoreText]);
+
+        return scoreField;
+    }
+
+    spawnVeggie() {
+        const veggieNames = ['BellPepper', 'Broccoli', 'Carrot', 'Cauliflower', 'Corn', 'Eggplant', 'GreenCabbage', 'Mushroom', 'Potato', 'Pumpkin', 'Radish', 'Squash', 'Tomato'];
+        const randomVeggieName = Phaser.Math.RND.pick(veggieNames);
+        const veggie = this.add.image(Phaser.Math.Between(50, this.sys.game.config.width - 50), -100, randomVeggieName)
+            .setOrigin(0.5, 0.5);
+
+        veggie.hasContributedToScore = false;
+
+        this.physics.world.enable(veggie);
+        this.veggieGroup.add(veggie);
+
+        const path = {
+            t: 0,
+            duration: 5000,
+            yoyo: false,
+            repeat: 0,
+            points: [
+                new Phaser.Math.Vector2(veggie.x, veggie.y),
+                new Phaser.Math.Vector2(veggie.x + 100, veggie.y - 200),
+                new Phaser.Math.Vector2(veggie.x + 200, this.sys.game.config.height + 100),
+            ],
+        };
+
+        this.tweens.add({
+            targets: path,
+            t: 1,
+            onUpdate: (tween) => {
+                const point = this.getPointOnPath(path, tween.getValue());
+                veggie.x = point.x;
+                veggie.y = point.y;
+
+                if (!veggie.hasContributedToScore && this.checkCollision(veggie)) {
+                    veggie.hasContributedToScore = true;
+                    veggie.destroy();
+                    score += 1;
+                    this.scoreField.getAt(1).setText(`Score: ${score}`);
+                }
+
+                if (!veggie.hasContributedToScore && veggie.y >= this.sys.game.config.height) {
+                    veggie.hasContributedToScore = true;
+                    veggie.destroy();
+                    const firstHeart = this.hearts.find(heart => !heart.isBroken);
+                    if (firstHeart) {
+                        firstHeart.setTexture('BrokenHeart');
+                        firstHeart.isBroken = true;
+                    }
+
+                    if (this.hearts.every(heart => heart.isBroken)) {
+                        this.scene.start('GameOver', { mode: 'life', score: score });
+                    }
+                }
+            },
+            duration: path.duration,
+        });
+
+        veggie.setRotation(Phaser.Math.DegToRad(Phaser.Math.Between(0, 360)));
+    }
+
+    handleMouseMove(pointer) {
+        this.trailPoints.push({ x: pointer.x, y: pointer.y });
+
+        while (this.trailPoints.length > 5) {
+            this.trailPoints.shift();
+        }
+
+        this.trailGraphics.clear();
+        this.trailGraphics.lineStyle(trailSize, 0x00ff00, 1);
+        this.trailGraphics.beginPath();
+        this.trailGraphics.moveTo(this.trailPoints[0].x, this.trailPoints[0].y);
+
+        for (let i = 1; i < this.trailPoints.length; i++) {
+            this.trailGraphics.lineTo(this.trailPoints[i].x, this.trailPoints[i].y);
+        }
+
+        this.trailGraphics.strokePath();
+    }
+
+    checkCollision(veggie) {
+        for (let i = 0; i < this.trailPoints.length - 1; i++) {
+            const p1 = this.trailPoints[i];
+            const p2 = this.trailPoints[i + 1];
+
+            if (
+                Phaser.Geom.Intersects.LineToRectangle(
+                    new Phaser.Geom.Line(p1.x, p1.y, p2.x, p2.y),
+                    veggie.getBounds()
+                )
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    getPointOnPath(path, t) {
+        const point = new Phaser.Math.Vector2();
+
+        const p0 = path.points[0];
+        const p1 = path.points[1];
+        const p2 = path.points[2];
+
+        point.x = Math.pow(1 - t, 2) * p0.x + 2 * (1 - t) * t * p1.x + Math.pow(t, 2) * p2.x;
+        point.y = Math.pow(1 - t, 2) * p0.y + 2 * (1 - t) * t * p1.y + Math.pow(t, 2) * p2.y;
+
+        return point;
     }
 }
 
 class GameOver extends Phaser.Scene {
     constructor() { super({ key: 'GameOver' }); }
 
+    init(data) {
+        this.gameMode = data.mode;
+        this.score = data.score;
+    }
+
     create() {
         console.log('GameOver: creating');
 
-        switch (mode) {
-            case "practice":
-                break;
-            case "timed":
-                break;
-            case "life":
+        const formattedGameMode = this.gameMode.charAt(0).toUpperCase() + this.gameMode.slice(1);
 
-        }
+        this.add.text(this.sys.game.config.width / 2, 200, formattedGameMode, {
+            fontSize: '120px',
+            fill: '#fff',
+        }).setOrigin(0.5);
+
+        this.add.text(this.sys.game.config.width / 2, 200, score, {
+            fontSize: '120px',
+            fill: '#fff',
+        }).setOrigin(0.5);
     }
 }
 
@@ -616,6 +858,7 @@ const config = {
         },
     },
     scene: [
+        LoadingScene,
         MainMenu,
         Intermission,
         PracticeGame,
