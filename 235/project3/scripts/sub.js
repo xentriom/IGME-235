84ee25d51675805fa5dc0cd0c50ee7fcba2bb64d
Jason Chen <c04jason@gmail.com>
2023-12-06 16:12:50 -0500

    added mode variables and created timed mode

--- 235/project3/scripts/sub.js
+++ 235/project3/scripts/sub.js
@@ -1,3 +1,5 @@
+const lives = 3;
+let mode = "";
 let score = 0;
 let trailSize = 2;
 
@@ -204,6 +206,7 @@ class PracticeGame extends Phaser.Scene {
 
     create() {
         console.log('PracticeGame: creating');
+        mode = "practice";
 
         this.createHomeButton();
         this.scoreField = this.createScoreField();
@@ -356,10 +359,206 @@ class PracticeGame extends Phaser.Scene {
 }
 
 class TimedGame extends Phaser.Scene {
-    constructor() { super({ key: 'TimedGame' }); }
+    constructor() {
+        super({ key: 'TimedGame' });
+        this.veggieGroup;
+        this.trailGraphics;
+        this.trailPoints = [];
+        this.currentTime = 60000;
+        this.timerEvent;
+    }
 
     create() {
         console.log('TimedGame: creating');
+        mode = "timed";
+
+        this.createHomeButton();
+        this.scoreField = this.createScoreField();
+        this.timerContainer = this.createTimer();
+
+        this.veggieGroup = this.physics.add.group();
+
+        this.spawnVeggie();
+        this.time.addEvent({
+            delay: 500,
+            callback: this.spawnVeggie,
+            callbackScope: this,
+            loop: true,
+        });
+
+        this.trailGraphics = this.add.graphics();
+        this.input.on('pointermove', this.handleMouseMove, this);
+
+        this.timerEvent = this.time.addEvent({
+            delay: 1000,
+            callback: this.updateTimer,
+            callbackScope: this,
+            loop: true,
+        });
+    }
+
+    createHomeButton() {
+        const homeButton = this.add.container(20, 20);
+        const iconKey = 'Home';
+
+        const buttonBackground = this.add.rectangle(10, 10, 50, 50, 0xffffff)
+            .setOrigin(0)
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => {
+                this.scene.start('MainMenu');
+            });
+
+        const homeIcon = this.add.image(35, 35, iconKey)
+            .setOrigin(0.5)
+            .setScale(2);
+
+        homeButton.add([buttonBackground, homeIcon]);
+
+        return homeButton;
+    }
+
+    createScoreField() {
+        score = 0;
+        const scoreField = this.add.container(this.sys.game.config.width - 20, 20);
+
+        const fieldBackground = this.add.graphics()
+            .fillStyle(0xffffff)
+            .fillRoundedRect(-220, 0, 200, 50, 10);
+
+        const scoreText = this.add.text(-50, 25, `Score: ${score}`, {
+            fontSize: '24px',
+            fill: '#000',
+        }).setOrigin(1, 0.5);
+
+        scoreField.add([fieldBackground, scoreText]);
+
+        return scoreField;
+    }
+
+    createTimer() {
+        const timerContainer = this.add.container(this.sys.game.config.width / 2, 20);
+
+        const timerBackground = this.add.graphics()
+            .fillStyle(0xffffff)
+            .fillRoundedRect(-100, 0, 200, 50, 10);
+
+        const timerText = this.add.text(0, 25, 'Time: 1:00', {
+            fontSize: '24px',
+            fill: '#000',
+        }).setOrigin(0.5, 0.5);
+
+        timerContainer.add([timerBackground, timerText]);
+
+        return timerContainer;
+    }
+
+    updateTimer() {
+        this.currentTime -= 1000;
+
+        const minutes = Math.floor(this.currentTime / 60000);
+        const seconds = Math.floor((this.currentTime % 60000) / 1000);
+        const formattedTime = `${minutes}:${String(seconds).padStart(2, '0')}`;
+
+        this.timerContainer.getAt(1).setText(`${formattedTime}`);
+
+        if (this.currentTime <= 0) {
+            this.scene.start('GameOver');
+            this.timerEvent.destroy();
+        }
+    }
+
+    spawnVeggie() {
+        const veggieNames = ['BellPepper', 'Broccoli', 'Carrot', 'Cauliflower', 'Corn', 'Eggplant', 'GreenCabbage', 'Mushroom', 'Potato', 'Pumpkin', 'Radish', 'Squash', 'Tomato'];
+        const randomVeggieName = Phaser.Math.RND.pick(veggieNames);
+        const veggie = this.add.image(Phaser.Math.Between(50, this.sys.game.config.width - 50), -100, randomVeggieName)
+            .setOrigin(0.5, 0.5);
+
+        veggie.hasContributedToScore = false;
+
+        this.physics.world.enable(veggie);
+        this.veggieGroup.add(veggie);
+
+        const path = {
+            t: 0,
+            duration: 5000,
+            yoyo: false,
+            repeat: 0,
+            points: [
+                new Phaser.Math.Vector2(veggie.x, veggie.y),
+                new Phaser.Math.Vector2(veggie.x + 100, veggie.y - 200),
+                new Phaser.Math.Vector2(veggie.x + 200, this.sys.game.config.height + 100),
+            ],
+        };
+
+        this.tweens.add({
+            targets: path,
+            t: 1,
+            onUpdate: (tween) => {
+                const point = this.getPointOnPath(path, tween.getValue());
+                veggie.x = point.x;
+                veggie.y = point.y;
+
+                if (!veggie.hasContributedToScore && this.checkCollision(veggie)) {
+                    veggie.hasContributedToScore = true;
+                    veggie.destroy();
+                    score += 1;
+                    this.scoreField.getAt(1).setText(`Score: ${score}`);
+                }
+            },
+            duration: path.duration,
+        });
+
+        veggie.setRotation(Phaser.Math.DegToRad(Phaser.Math.Between(0, 360)));
+    }
+
+    handleMouseMove(pointer) {
+        this.trailPoints.push({ x: pointer.x, y: pointer.y });
+
+        while (this.trailPoints.length > 5) {
+            this.trailPoints.shift();
+        }
+
+        this.trailGraphics.clear();
+        this.trailGraphics.lineStyle(trailSize, 0x00ff00, 1);
+        this.trailGraphics.beginPath();
+        this.trailGraphics.moveTo(this.trailPoints[0].x, this.trailPoints[0].y);
+
+        for (let i = 1; i < this.trailPoints.length; i++) {
+            this.trailGraphics.lineTo(this.trailPoints[i].x, this.trailPoints[i].y);
+        }
+
+        this.trailGraphics.strokePath();
+    }
+
+    checkCollision(veggie) {
+        for (let i = 0; i < this.trailPoints.length - 1; i++) {
+            const p1 = this.trailPoints[i];
+            const p2 = this.trailPoints[i + 1];
+
+            if (
+                Phaser.Geom.Intersects.LineToRectangle(
+                    new Phaser.Geom.Line(p1.x, p1.y, p2.x, p2.y),
+                    veggie.getBounds()
+                )
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    getPointOnPath(path, t) {
+        const point = new Phaser.Math.Vector2();
+
+        const p0 = path.points[0];
+        const p1 = path.points[1];
+        const p2 = path.points[2];
+
+        point.x = Math.pow(1 - t, 2) * p0.x + 2 * (1 - t) * t * p1.x + Math.pow(t, 2) * p2.x;
+        point.y = Math.pow(1 - t, 2) * p0.y + 2 * (1 - t) * t * p1.y + Math.pow(t, 2) * p2.y;
+
+        return point;
     }
 }
 
@@ -368,6 +567,7 @@ class LifeGame extends Phaser.Scene {
 
     create() {
         console.log('LifeGame: creating');
+        mode = "life";
     }
 }
 
@@ -376,6 +576,15 @@ class GameOver extends Phaser.Scene {
 
     create() {
         console.log('GameOver: creating');
+
+        switch (mode) {
+            case "practice":
+                break;
+            case "timed":
+                break;
+            case "life":
+
+        }
     }
 }
 
